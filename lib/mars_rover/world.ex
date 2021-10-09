53d83ce83aa7eeadf1b1481a7d022d5cb32e4b27
Sam Frances <sam@samfrances.uk>
2021-10-09 10:29:05 +0100

    Write logic for checking if vector is within world

--- lib/mars_rover/world.ex
+++ lib/mars_rover/world.ex
@@ -1,10 +1,20 @@
 defmodule MarsRover.World do
 
-  @opaque t :: {pos_integer(), pos_integer()}
+  defstruct [
+    width: 1,
+    height: 1,
+  ]
+
+  defguard contains?(world, vector) when vector.x >= 0 and vector.y >= 0 and vector.x <= world.width and vector.y <= world.height
+
+  @opaque t :: %__MODULE__{
+    width: pos_integer(),
+    height: pos_integer()
+  }
 
   @spec new(pos_integer(), pos_integer()) :: t()
   def new(width, height) when width > 0 and height > 0 do
-    {width, height}
+    %__MODULE__{width: width, height: height}
   end
   def new(_width, _height) do
     {:error, "World must have a positive width and height"}
--- test/world_test.exs
+++ test/world_test.exs
@@ -1,7 +1,9 @@
 defmodule WorldTest do
   use ExUnit.Case
 
+  require MarsRover.World
   alias MarsRover.World
+  alias MarsRover.Vector2D
 
   test "cannot create empty world" do
     assert {:error, _message} = World.new(0, 0)
@@ -28,5 +30,58 @@ defmodule WorldTest do
   test "can create larger world" do
     refute match?({:error, _message}, World.new(100, 44))
   end
+
+  [
+    {World.new(1, 1), Vector2D.new(0, 0), true},
+    {World.new(1, 1), Vector2D.new(1, 0), true},
+    {World.new(1, 1), Vector2D.new(0, 1), true},
+    {World.new(1, 1), Vector2D.new(1, 1), true},
+    {World.new(1, 1), Vector2D.new(-1, 0), false},
+    {World.new(1, 1), Vector2D.new(0, -1), false},
+    {World.new(1, 1), Vector2D.new(-1, -1), false},
+    {World.new(1, 1), Vector2D.new(0, 2), false},
+    {World.new(1, 1), Vector2D.new(2, 0), false},
+    {World.new(1, 1), Vector2D.new(2, 2), false},
+    {World.new(10, 10), Vector2D.new(0, 0), true},
+    {World.new(10, 10), Vector2D.new(1, 0), true},
+    {World.new(10, 10), Vector2D.new(0, 1), true},
+    {World.new(10, 10), Vector2D.new(1, 1), true},
+    {World.new(10, 10), Vector2D.new(5, 0), true},
+    {World.new(10, 10), Vector2D.new(0, 5), true},
+    {World.new(10, 10), Vector2D.new(5, 5), true},
+    {World.new(10, 10), Vector2D.new(6, 1), true},
+    {World.new(10, 10), Vector2D.new(1, 6), true},
+    {World.new(10, 10), Vector2D.new(6, 6), true},
+    {World.new(10, 10), Vector2D.new(9, 0), true},
+    {World.new(10, 10), Vector2D.new(0, 9), true},
+    {World.new(10, 10), Vector2D.new(9, 9), true},
+    {World.new(10, 10), Vector2D.new(10, 0), true},
+    {World.new(10, 10), Vector2D.new(0, 10), true},
+    {World.new(10, 10), Vector2D.new(10, 10), true},
+    {World.new(10, 10), Vector2D.new(11, 0), false},
+    {World.new(10, 10), Vector2D.new(0, 11), false},
+    {World.new(10, 10), Vector2D.new(11, 11), false},
+    {World.new(10, 10), Vector2D.new(11, 2), false},
+    {World.new(10, 10), Vector2D.new(2, 11), false},
+    {World.new(10, 10), Vector2D.new(-1, 0), false},
+    {World.new(10, 10), Vector2D.new(0, -1), false},
+    {World.new(10, 10), Vector2D.new(-1, -1), false},
+    {World.new(10, 10), Vector2D.new(-1, 7), false},
+    {World.new(10, 10), Vector2D.new(7, -1), false},
+    {World.new(10, 10), Vector2D.new(-3, 0), false},
+    {World.new(10, 10), Vector2D.new(0, -3), false},
+    {World.new(10, 10), Vector2D.new(-3, 2), false},
+    {World.new(10, 10), Vector2D.new(2, -3), false},
+    {World.new(10, 10), Vector2D.new(-3, 12), false},
+    {World.new(10, 10), Vector2D.new(12, -3), false},
+  ]
+  |> Enum.each(fn {world, vector, within} ->
+    @tag world: world
+    @tag vector: vector
+    @tag within: within
+    test "#{inspect vector} is#{if within, do: "", else: " not"} within #{inspect world}", ctx do
+      assert World.contains?(ctx.world, ctx.vector) == ctx.within
+    end
+  end)
 
 end
